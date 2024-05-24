1de0d297e444e9318a0807e7e0e045509a45232e
Michael Vega <micha3lvega@gmail.com>
2024-05-24 14:10:37 -0500

    Inicializacion de proyecto

---
+++ src/main/java/co/com/micha3lvega/store/CleanArchStoreApplication.java
@@ -0,0 +1,13 @@
+package co.com.micha3lvega.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchStoreApplication.class, args);
+	}
+
+}
